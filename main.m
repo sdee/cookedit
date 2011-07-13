7a006de78f8ba67bc5584dcc2ec810a66d8648ed
Adam Robertson <adam.i.robertson@gmail.com>
2011-07-13 16:16:54 -0700

    Adding project to repo

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cookedit
+//
+//  Created by Adam Robertson on 6/27/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
